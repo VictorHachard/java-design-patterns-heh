79cbc301cb5b6c7bfb014b5671bb961fb8b5326f
VictorHachard <nicolashachard@gmail.com>
2021-07-05 21:21:55 +0200

    Remove duplicated code

--- TP4bis/EX3/Code/src/be/isims/ihm/tp4bis/ex3/Controller.java
+++ TP4bis/EX3/Code/src/be/isims/ihm/tp4bis/ex3/Controller.java
@@ -15,10 +15,11 @@ public class Controller implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         String buttonText = ((JButton) actionEvent.getSource()).getText();
-        if (buttonText.equals("Raise"))
-            temperatureModelView.getModel().setTemperature(temperatureModelView.getModel().getTemperature() + 1);
-        else if (buttonText.equals("Lower"))
-            temperatureModelView.getModel().setTemperature(temperatureModelView.getModel().getTemperature() - 1);
+        if (buttonText.equals("Raise")) {
+          temperatureModelView.getModel().setTemperature(temperatureModelView.getModel().getTemperature() + 1);
+        } else if (buttonText.equals("Lower")) {
+          temperatureModelView.getModel().setTemperature(temperatureModelView.getModel().getTemperature() - 1);
+        }
         temperatureModelView.setTextLabel(Double.toString(temperatureModelView.getModel().getTemperature()));
     }
 
